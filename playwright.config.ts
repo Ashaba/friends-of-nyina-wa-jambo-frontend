735d26b71898e53341b1e5c3a4cd0398cfc1261f
ashaba <jnashaba4@gmail.com>
2025-12-18 12:39:41 -0600

    fix ci playwright runner

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   webServer: {
     command: "npm run dev",
     url: `http://localhost:${PORT}`,
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true,
     timeout: 120000,
   },
 });
